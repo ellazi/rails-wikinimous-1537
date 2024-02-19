4876896ca1fc99003a015174fa300fe5745648fa
rossella pellizzi <prellizzi@gmail.com>
2024-02-19 11:48:27 +0100

    this was fun

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "articles#index"
+  resources :articles, except: :index
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts "Creating interesting articles...be patient..."
+
+10.times do |i|
+  Article.create(
+    title: Faker::Dessert.flavor,
+    content: Faker::Lorem.paragraphs(number: 3).join("\n")
+  )
+end
+
+puts "Created #{Article.count} articles."
